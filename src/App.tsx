4efafbb5333448a2397a26d6889729ed3e542911
Carol Guo <fangyi.guo@sydney.edu.au>
2023-09-17 19:26:31 +1000

    responsive

--- src/App.tsx
+++ src/App.tsx
@@ -54,9 +54,9 @@ function App() {
 
   return (
     <div className="App">
-      <header className={windowDimensions.width > breakpoints.md ? "App-header py-20 pl-20 pr-4": "App-header"}>
-        <div className='flex flex-row'>
-          <div className='flex flex-col justify-center ml-12'>
+      <header className={windowDimensions.width > breakpoints.md ? "App-header py-20 pl-20 pr-4": "App-header py-24"}>
+        <div className={windowDimensions.width > breakpoints.md ? 'flex flex-row' : "flex flex-col" }>
+          <div className={windowDimensions.width > breakpoints.md ? 'flex flex-col justify-center ml-12' : 'flex flex-row justify-center mx-8'}>
             <div className='flex flex-col text-start'>
               <h1 className="text-5xl text-primaryText font-MontserratSemiBold mb-6">
                 Hello World! I'm Carol!
@@ -67,16 +67,16 @@ function App() {
               <p className='text-xl text-primaryText font-MontserratMedium mb-3'>
                 I work as a full-stack developer specializing in React Native and React.js. with a knack for turning coffee into code and debugging into an art form. I'm just an email away. Reach out and let's chat!
               </p>
-            </div>
-            <div className='flex flex-row justify-start w-full'>
-              <img src={resumeIcon} onClick={() => {window.open('https://drive.google.com/file/d/1RdRATEy3eyS6HXnd7_b5Ok-RtwXBCPQ3/view?usp=sharing', '_blank');}} className='my-3 mr-3 hover:cursor-pointer'/>
-              <img src={githubIcon} onClick={() => {window.open('https://github.com/CarolFYGuo', '_blank');}} className='m-3 hover:cursor-pointer'/>
-              <img src={linkedInIcon} onClick={() => {window.open('https://www.linkedin.com/in/carol-guo-59229a231/', '_blank');}} className='m-3 hover:cursor-pointer'/>
-              <img src={emailIcon} onClick={() => {window.open('https://outlook.live.com/mail/0/deeplink/compose?mailtouri=mailto%3Acarolguounsw%40gmail.com', '_blank');}} className='m-3 hover:cursor-pointer'/>
-              <img src={facebookIcon} onClick={() => {window.open('https://www.facebook.com/lilylilylilyguo', '_blank');}} className='m-3 hover:cursor-pointer'/>
+              <div className='flex flex-row justify-start w-full'>
+                <img src={resumeIcon} onClick={() => {window.open('https://drive.google.com/file/d/1RdRATEy3eyS6HXnd7_b5Ok-RtwXBCPQ3/view?usp=sharing', '_blank');}} className='my-3 mr-3 hover:cursor-pointer'/>
+                <img src={githubIcon} onClick={() => {window.open('https://github.com/CarolFYGuo', '_blank');}} className='m-3 hover:cursor-pointer'/>
+                <img src={linkedInIcon} onClick={() => {window.open('https://www.linkedin.com/in/carol-guo-59229a231/', '_blank');}} className='m-3 hover:cursor-pointer'/>
+                <img src={emailIcon} onClick={() => {window.open('https://outlook.live.com/mail/0/deeplink/compose?mailtouri=mailto%3Acarolguounsw%40gmail.com', '_blank');}} className='m-3 hover:cursor-pointer'/>
+                <img src={facebookIcon} onClick={() => {window.open('https://www.facebook.com/lilylilylilyguo', '_blank');}} className='m-3 hover:cursor-pointer'/>
+              </div>
             </div>
           </div>
-          {windowDimensions.width > breakpoints.md  && <img src={myself} className='h-[80vh]'/>}
+          <img src={myself} className={windowDimensions.width > breakpoints.md ? 'h-[80vh]' : 'mt-4'} />
         </div>
       </header>
     </div>
