2c3096edbfffab4a563af40e332b72c7d708c322
Carol Guo <fangyi.guo@sydney.edu.au>
2024-12-11 23:03:54 +1030

    update resume

--- src/App.tsx
+++ src/App.tsx
@@ -70,7 +70,7 @@ function App() {
                 I work as a full-stack developer specializing in React Native and React.js. with a knack for turning coffee into code and debugging into an art form. I'm just an email away. Reach out and let's chat!
               </p>
               <div className='flex flex-row justify-start w-full'>
-                <img src={resumeIcon} onClick={() => {window.open('https://drive.google.com/file/d/1RdRATEy3eyS6HXnd7_b5Ok-RtwXBCPQ3/view?usp=sharing', '_blank');}} className='my-3 mr-3 hover:cursor-pointer'/>
+                <img src={resumeIcon} onClick={() => {window.open('https://drive.google.com/file/d/13uzbWQdc1pD4_Ju6Fl_j8Y6BPCttLxHa/view?usp=share_link', '_blank');}} className='my-3 mr-3 hover:cursor-pointer'/>
                 <img src={githubIcon} onClick={() => {window.open('https://github.com/CarolFYGuo', '_blank');}} className='m-3 hover:cursor-pointer'/>
                 <img src={linkedInIcon} onClick={() => {window.open('https://www.linkedin.com/in/carol-guo-59229a231/', '_blank');}} className='m-3 hover:cursor-pointer'/>
                 <img src={emailIcon} onClick={() => {window.open('https://outlook.live.com/mail/0/deeplink/compose?mailtouri=mailto%3Acarolguounsw%40gmail.com', '_blank');}} className='m-3 hover:cursor-pointer'/>
